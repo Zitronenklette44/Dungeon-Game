9d0a2b871cafea3fb26e75aa65f502a4d84a09ad
Zitronenklette44 <110123264+Zitronenklette44@users.noreply.github.com>
2024-06-10 09:30:30 +0200

    MobTemplate.java aktualisieren

--- src/entitys/MobTemplate.java
+++ src/entitys/MobTemplate.java
@@ -8,6 +8,8 @@ public class MobTemplate extends Rechteck{
     public int SpawnX;
     public int SpawnY;
     public int speed;
+	public int damage;
+	public int Hp;
     
     public MobTemplate(int hoehe, int breite, int posX, int posY, int dx, int dy, int speed, int SpawnX, int SpawnY) {
         super(hoehe, breite, posX, posY);
