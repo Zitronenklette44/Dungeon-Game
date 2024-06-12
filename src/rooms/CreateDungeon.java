84f72b5d3e3883df7322d214497e4a1151db0ffb
Zitronenklette44 <110123264+Zitronenklette44@users.noreply.github.com>
2024-06-12 20:34:57 +0200

    int[] --> ArrayList<Integer>

--- src/rooms/CreateDungeon.java
+++ src/rooms/CreateDungeon.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import gui.Draw;
 import rooms.Home.*;
+import rooms.Village.Goal;
 import rooms.Village.HouseEntrance;
 import rooms.Village.HouseExit;
 import rooms.Village.HouseMiddel;
@@ -19,139 +20,151 @@ import rooms.Village.MarketMiddel;
 import rooms.Village.MarketMiddel1;
 import rooms.Village.MarketMiddel2;
 import rooms.Village.Normal;
+import rooms.Village.Spawn;
 
 public class CreateDungeon {
-	public int[] DungeonRooms;
-	public int DungeonLength;
-	public int currentRoom=0;
-	private static RoomTemplate[][] rooms = {{new Dungeon("Dungeon"), new Entrace("Eingang"), new Center("Mitte"), new Exit("Ausgang"), new Field("Field")},
-			{new Normal("Path"), new HouseEntrance("Haus eingang"), new HouseMiddel("Haus Mitte 1"), new HouseMiddel1("Haus Mitte 2"), new HouseMiddel2("Haus Mitte 3"), new HouseExit("Haus Exit"),
-					new MarketEntrance("Markt Eingang"), new MarketMiddel("Markt Mitte 1"), new MarketMiddel1("Markt Mitte 2"),new MarketMiddel2("Markt Mitte 3"), new MarketExit("Markt Exit")}};
-	private int[][] Features= {{1,6}};
-	private static int[] homeVillage = {0,1,2,3,4};
-	public boolean homeVillageBuild = true;
-	public static int dungeonType = 0;
-	private static int[] maxRooms = {10,20,50,100};
+    public ArrayList<Integer> DungeonRooms;
+    public int DungeonLength;
+    public int currentRoom = 0;
+    private static RoomTemplate[][] rooms = {
+            {new Dungeon("Dungeon"), new Entrace("Eingang"), new Center("Mitte"), new Exit("Ausgang"), new Field("Field")},
+            {new Normal("Path"), new HouseEntrance("Haus eingang"), new HouseMiddel("Haus Mitte 1"), new HouseMiddel1("Haus Mitte 2"), new HouseMiddel2("Haus Mitte 3"), new HouseExit("Haus Exit"),
+                    new MarketEntrance("Markt Eingang"), new MarketMiddel("Markt Mitte 1"), new MarketMiddel1("Markt Mitte 2"), new MarketMiddel2("Markt Mitte 3"), new MarketExit("Markt Exit"),
+                    new Spawn("Spawn"), new Goal("Goal")},
+            {}
+    };
+    private int[][] Features = {{1, 6}};
+    private static int[] homeVillage = {0, 1, 2, 3, 4};
+    public boolean homeVillageBuild = false;
+    public static int dungeonType = 1;
+    private static int[] maxRooms = {10, 20, 50, 100};
 
-	public void createDungeon() {
-		if(!homeVillageBuild) {
-		DungeonLength = (int)(Math.random()*(maxRooms[dungeonType]/2))+(maxRooms[dungeonType]/2);
-		DungeonRooms= new int[DungeonLength];
-		System.out.println("Dungeon erstelle mit:"+DungeonLength+" mit folgenden Räumen: ");
-		for(int i= 0; i<DungeonLength;i++) {
-			DungeonRooms[i]= -1;
-		}
-		spreadFeatures();
-		finishFeatures();
+    public void createDungeon() {
+        if (!homeVillageBuild) {
+            DungeonLength = (int) (Math.random() * (maxRooms[dungeonType-1] / 2)) + (maxRooms[dungeonType-1] / 2);
+            DungeonRooms = new ArrayList<Integer>();
+            System.out.println("Dungeon erstelle mit:" + DungeonLength + " mit folgenden Räumen: ");
+            for (int i = 0; i < DungeonLength; i++) {
+                DungeonRooms.add(-1);
+            }
+            spreadFeatures();
+            finishFeatures();
+            
+            DungeonRooms.addFirst(11);
+            DungeonRooms.addLast(12);
+            
+            DungeonLength+=2;
+            
+            for (int i = 0; i < DungeonLength; i++) {
+                System.out.print(DungeonRooms.get(i) + ", ");
+            }
+            
+        }
+    }
 
-		for(int i= 0; i<DungeonLength;i++) {
-			System.out.print(DungeonRooms[i]+", ");
-		}
-		}
-	}
+    private void spreadFeatures() {
+        int indexLastFeature = -100;
+        int random;
 
-	private void spreadFeatures() {
-		int indexLastFeature= -100;
-		int random;
+        for (int i = 0; i < DungeonLength; i++) {
+            if (i + 3 >= DungeonLength) {
+                break;
+            }
+            if (indexLastFeature + 3 > i) {
+            } else {
+                random = (int) (Math.random() * 100);
+                if (random > 50) {
+                    DungeonRooms.set(i, Features[dungeonType - 1][(int) (Math.random() * Features[dungeonType - 1].length)]);
+                    indexLastFeature = i;
+                }
+            }
+        }
 
-		for(int i = 0;i<DungeonLength;i++) {
-			if(i+3>=DungeonLength) {
-				break;
-			}
-			if(indexLastFeature+3>i) {
-			}else {
-				random = (int) (Math.random()*100);
-				if(random>50) {
-					DungeonRooms[i]=Features[dungeonType-1][(int) (Math.random()*Features[dungeonType-1].length)]; 
-					indexLastFeature = i;
-				}
-			}
-		}
+    }
 
-	}
-	
-	private void finishFeatures() {
-		for(int i=0;i<DungeonLength;i++) {
-			if(DungeonRooms[i]!=-1 &&checkFeature(DungeonRooms[i])) {
-				int maxFeatureLenght= 0;
-				for(int j = i+1;j<DungeonLength;j++) {
-					if(DungeonRooms[j]==-1) {
-						maxFeatureLenght++;
-					}else {break;}
-				}				
-				
-				int FeatureLength = (int) (Math.random()*(maxFeatureLenght)+1);
-				int FeatureEnd = DungeonRooms[i]+4;
-				DungeonRooms[FeatureLength+i] = FeatureEnd;
-				
-				int[] possibleRooms = {DungeonRooms[i]+1,DungeonRooms[i]+2,DungeonRooms[i]+3};
-				for(int j = i+1;j<FeatureLength+i;j++) {
-					DungeonRooms[j]= possibleRooms[(int) (Math.random()*3)];; 
-				}
-				
-			}
-		}
-		for(int i= 0;i<DungeonLength;i++) {
-			if(DungeonRooms[i]==-1) {
-				DungeonRooms[i]= 0;
-			}
-		}
-	}	
-	
-	private boolean checkFeature(int Feature) {
-		for(int i= 0;i<Features[dungeonType-1].length;i++) {
-			if(Features[dungeonType-1][i]==Feature) {
-				return true;
-			}
-			
-		}
-		return false;
+    private void finishFeatures() {
+        for (int i = 0; i < DungeonLength; i++) {
+            if (DungeonRooms.get(i) != -1 && checkFeature(DungeonRooms.get(i))) {
+                int maxFeatureLenght = 0;
+                for (int j = i + 1; j < DungeonLength; j++) {
+                    if (DungeonRooms.get(j) == -1) {
+                        maxFeatureLenght++;
+                    } else {
+                        break;
+                    }
+                }
 
-	}
+                int FeatureLength = (int) (Math.random() * (maxFeatureLenght) + 1);
+                int FeatureEnd = DungeonRooms.get(i) + 4;
+                DungeonRooms.set(FeatureLength + i, FeatureEnd);
 
+                int[] possibleRooms = {DungeonRooms.get(i) + 1, DungeonRooms.get(i) + 2, DungeonRooms.get(i) + 3};
+                for (int j = i + 1; j < FeatureLength + i; j++) {
+                    DungeonRooms.set(j, possibleRooms[(int) (Math.random() * 3)]);
+                }
 
-	public int getRoom() {
-		if(homeVillageBuild) {
-			return homeVillage[currentRoom];
-		}
-		return DungeonRooms[currentRoom];
-	}
+            }
+        }
+        for (int i = 0; i < DungeonLength; i++) {
+            if (DungeonRooms.get(i) == -1) {
+                DungeonRooms.set(i, 0);
+            }
+        }
+    }
 
-	public int getLastRoom() {
-		if(homeVillageBuild) {
-			return homeVillage[currentRoom-1];
-		}
-		return DungeonRooms[currentRoom-1];
-	}
+    private boolean checkFeature(int Feature) {
+        for (int i = 0; i < Features[dungeonType - 1].length; i++) {
+            if (Features[dungeonType - 1][i] == Feature) {
+                return true;
+            }
 
-	public int getNextRoom() {
-		if(homeVillageBuild) {
-			return homeVillage[currentRoom+1];
-		}
-		return DungeonRooms[currentRoom+1];
-	}
+        }
+        return false;
 
-	public int getDungeonLenght() {
-		if(homeVillageBuild) {
-			return homeVillage.length;
-		}
-		return DungeonLength;
-	}
+    }
 
-	public void drawRoom(Graphics2D g2d) {
-		Draw.clearObjects();
-		if(!homeVillageBuild) {
-		rooms[dungeonType][DungeonRooms[currentRoom]].changeColors();
-		g2d.setFont(new Font("Arial", Font.BOLD, 20));
-		g2d.drawString(rooms[dungeonType][DungeonRooms[currentRoom]].name, 200, 300);
-		rooms[dungeonType][DungeonRooms[currentRoom]].createObjects();
-		rooms[dungeonType][DungeonRooms[currentRoom]].DrawImage(g2d);
-		}else {
-			rooms[dungeonType][currentRoom].changeColors();
-			g2d.setFont(new Font("Arial", Font.BOLD, 20));
-			g2d.drawString(rooms[dungeonType][currentRoom].name, 200, 300);
-			rooms[dungeonType][currentRoom].createObjects();
-			rooms[dungeonType][currentRoom].DrawImage(g2d);
-		}
-	}
+    public int getRoom() {
+        if (homeVillageBuild) {
+            return homeVillage[currentRoom];
+        }
+        return DungeonRooms.get(currentRoom);
+    }
+
+    public int getLastRoom() {
+        if (homeVillageBuild) {
+            return homeVillage[currentRoom - 1];
+        }
+        return DungeonRooms.get(currentRoom - 1);
+    }
+
+    public int getNextRoom() {
+        if (homeVillageBuild) {
+            return homeVillage[currentRoom + 1];
+        }
+        return DungeonRooms.get(currentRoom + 1);
+    }
+
+    public int getDungeonLenght() {
+        if (homeVillageBuild) {
+            return homeVillage.length;
+        }
+        return DungeonLength;
+    }
+
+    public void drawRoom(Graphics2D g2d) {
+        Draw.clearObjects();
+        if (!homeVillageBuild) {
+            rooms[dungeonType][DungeonRooms.get(currentRoom)].changeColors();
+            g2d.setFont(new Font("Arial", Font.BOLD, 20));
+            g2d.drawString(rooms[dungeonType][DungeonRooms.get(currentRoom)].name, 200, 300);
+            rooms[dungeonType][DungeonRooms.get(currentRoom)].createObjects();
+            rooms[dungeonType][DungeonRooms.get(currentRoom)].DrawImage(g2d);
+        } else {
+            rooms[dungeonType][currentRoom].changeColors();
+            g2d.setFont(new Font("Arial", Font.BOLD, 20));
+            g2d.drawString(rooms[dungeonType][currentRoom].name, 200, 300);
+            rooms[dungeonType][currentRoom].createObjects();
+            rooms[dungeonType][currentRoom].DrawImage(g2d);
+        }
+    }
 }
---
+++ src/rooms/Village/Goal.java
@@ -0,0 +1,12 @@
+package rooms.Village;
+
+import rooms.RoomTemplate;
+
+public class Goal extends RoomTemplate{
+
+	public Goal(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
